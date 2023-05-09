1e76fed9a77de6d42d63e5b27dde8522bf294692
DamianHalpe <damian.halpe@gmail.com>
2023-05-09 14:25:06 +0400

    Math extension functions implemented

---
+++ UtilityExt.Test/MathXTest.cs
@@ -0,0 +1,94 @@
+﻿namespace UtilityExt.Test
+{
+    [TestClass]
+    public class MathXTest
+    {
+        [DataTestMethod]
+        [DataRow(null, 0)]
+        [DataRow(double.MinValue, double.MinValue)]
+        [DataRow(double.MaxValue, double.MaxValue)]
+        public void TestZeroIfNull(double? value, double checkValue)
+        {
+            var returnValue = value.ZeroIfNull();
+            Assert.AreEqual(checkValue, returnValue);
+        }
+
+        [DataTestMethod]
+        [DataRow(100.011, 2, "100.01")]
+        [DataRow(100.011, 0, "100")]
+        [DataRow(100.010, 3, "100.010")]
+        [DataRow(null, 3, null)]
+        public void TestRoundDecimals(double? value, int decimals, string checkValue)
+        {
+            var returnValue = value.RoundDecimals(decimals);
+            if (checkValue == null)
+            {
+                Assert.AreEqual(null, returnValue);
+            }
+            else
+            {
+                decimal.TryParse(checkValue, out decimal checkVal);
+                Assert.AreEqual(returnValue, checkVal);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(100.11, 100.02, "200.13")]
+        [DataRow(100.0, 100.02, "200.02")]
+        [DataRow(100.01, 100.0, "200.01")]
+        [DataRow(100.01, null, "100.01")]
+        [DataRow(null, 200.01, "200.01")]
+        [DataRow(null, null, "0")]
+        public void TestAddDoubles(double? value1, double? value2, string checkValue)
+        {
+            var returnValue = MathX.AddDoubles(value1, value2);
+            decimal.TryParse(checkValue, out decimal checkVal);
+            Assert.AreEqual(checkVal, returnValue);
+        }
+
+        [DataTestMethod]
+        [DataRow(10.0, 2, "0.10")]
+        [DataRow(100.0, 3, "0.010")]
+        [DataRow(3.0, 3, "0.333")]
+        [DataRow(null, 2, null)]
+        public void TestInverseValue(double? value, int roundTo,  string checkValue)
+        {
+            var returnValue = MathX.InverseValue(value, roundTo);
+            if (checkValue == null)
+            {
+                Assert.AreEqual(null, returnValue);
+            }
+            else
+            {
+                decimal.TryParse(checkValue, out decimal checkVal);
+                Assert.AreEqual(returnValue, checkVal);
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(null, 2, 2)]
+        [DataRow(2, null, 2)]
+        [DataRow(null, null, null)]
+        public void TestAddInts(int? value1, int? value2, int? checkValue)
+        {
+            var returnValue = MathX.AddInts(value1, value2);
+            Assert.AreEqual(checkValue, returnValue);
+        }
+
+        [DataTestMethod]
+        [DataRow(1.0, 2.0, 3.0 , "6.0")]
+        [DataRow(1.0, 2.0, null, "3.0")]
+        [DataRow(null, null, null, "0.0")]
+        public void TestSumDoubles(double? value1, double? value2, double? value3, string checkValue)
+        {
+            var array = new List<double?>();
+            array.Add(value1);
+            array.Add(value2);
+            array.Add(value3);
+            var returnValue = array.SumDoubles();
+            decimal.TryParse(checkValue, out decimal checkVal);
+            Assert.AreEqual(returnValue, checkVal);
+        }
+    }
+}
---
+++ UtilityExt/MathX.cs
@@ -0,0 +1,89 @@
+﻿namespace UtilityExt
+{
+    public static class MathX
+    {
+        public static double ZeroIfNull(this double? value)
+        {
+            return value ?? 0;
+        }
+
+        public static decimal? RoundDecimals(this double? value, int decimals)
+        {
+            if (value != null) return RoundDecimals((double)value, decimals);
+            return null;
+        }
+
+        public static decimal? RoundDecimals(this double value, int decimals)
+        {
+            if (!double.IsInfinity(value)) return Math.Round((decimal)value, decimals);
+            return null;
+        }
+
+        public static string? FormatDecimals(this decimal? value, string format)
+        {
+            if (value != null) return ((decimal)value).ToString(format);
+            return null;
+        }
+
+        public static decimal AddDoubles(double? value1, double? value2)
+        {
+            var double1 = value1.ZeroIfNull();
+            var double2 = value2.ZeroIfNull();
+            if (double.IsInfinity(double1)) double1 = 0;
+            if (double.IsInfinity(double2)) double2 = 0;
+            return (decimal)double1 + (decimal)double2;
+        }
+
+        public static double? RoundUpDouble(this double? value, int decimals)
+        {
+            var result = RoundDouble(value, decimals);
+            if (result != null && result < value) result = Convert.ToDouble(AddDoubles(result, Math.Pow(10, -decimals)));
+            return result;
+        }
+
+        public static double? RoundDouble(this double? value, int decimals)
+        {
+            var result = value.RoundDecimals(decimals);
+            if (result != null) return Convert.ToDouble(result);
+            return null;
+        }
+
+        public static decimal RoundDecimals(this decimal value, int decimals)
+        {
+            return Math.Round(value, decimals);
+        }
+
+        public static decimal? RoundDecimals(this decimal? value, int decimals)
+        {
+            if (value == null) return null;
+            return Math.Round((decimal)value, decimals);
+        }
+
+        public static int? AddInts(int? value1, int? value2)
+        {
+            if (value1 == null) return value2;
+            if (value2 == null) return value1;
+            return value1 + value2;
+        }
+
+        public static decimal? InverseValue(double? doubleValue, int roundTo = 2)
+        {
+            if (doubleValue != null && doubleValue != 0) return (1 / doubleValue).RoundDecimals(roundTo);
+            return null;
+        }
+
+        public static decimal SumDoubles(this IEnumerable<double?> collection)
+        {
+            decimal total = 0;
+
+            if (collection == null) return total;
+
+            foreach (var value in collection)
+            {
+                if (value != null) total += (decimal)value;
+            }
+
+            return total;
+        }
+    }
+}
